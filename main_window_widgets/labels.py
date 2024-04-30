4b22167fd7715a96bf88d9c55191fe753afb6d5c
Boyko Filipov <filipov.boyko@gmail.com>
2024-04-30 22:09:58 +0300

    Add additional option to select a team from a specific client

--- main_window_widgets/labels.py
+++ main_window_widgets/labels.py
@@ -11,21 +11,27 @@ class MainWindowLabels(customtkinter.CTkLabel):
                                                          font=("halvetika", 15))
         self.measure_type_label.place(x=1, y=220)
 
-        self.select_team_label = customtkinter.CTkLabel(master, text="Client:", fg_color="grey20", width=150,
+        self.select_client_label = customtkinter.CTkLabel(master, text="Client:", fg_color="grey20", width=150,
+                                                          font=("halvetika", 15))
+        self.select_client_label.place(x=650, y=1)
+
+        self.select_team_label = customtkinter.CTkLabel(master, text="Team:", fg_color="grey20", width=150,
                                                         font=("halvetika", 15))
-        self.select_team_label.place(x=650, y=1)
+        self.select_team_label.place(x=650, y=80)
 
-        self.select_team_person = customtkinter.CTkLabel(master, text="Team member:", fg_color="grey20", width=150,
-                                                         font=("halvetika", 15))
-        self.select_team_person.place(x=650, y=80)
+        self.select_team_member_label = customtkinter.CTkLabel(master, text="Team member:", fg_color="grey20",
+                                                               width=150,
+                                                               font=("halvetika", 15))
+        self.select_team_member_label.place(x=650, y=160)
 
-        self.comments_label = customtkinter.CTkLabel(master, text="", fg_color="grey20", corner_radius=6, width=350, height=40,
-                                                         font=("halvetika", 10))
+        self.comments_label = customtkinter.CTkLabel(master, text="", fg_color="grey20", corner_radius=6, width=350,
+                                                     height=40,
+                                                     font=("halvetika", 10))
         self.comments_label.place(x=155, y=150)
 
-        self.select_team_process = customtkinter.CTkLabel(master, text="Process:", fg_color="grey20", width=150,
-                                                         font=("halvetika", 15))
-        self.select_team_process.place(x=650, y=160)
+        self.select_team_process_label = customtkinter.CTkLabel(master, text="Process:", fg_color="grey20", width=150,
+                                                                font=("halvetika", 15))
+        self.select_team_process_label.place(x=650, y=240)
 
         self.timer_label = customtkinter.CTkLabel(master, textvariable=master.stopwatch.time_var, fg_color="grey40",
                                                   width=300,
--- main_window_widgets/option_menus.py
+++ main_window_widgets/option_menus.py
@@ -5,14 +5,18 @@ class MainWindowOptionMenus(customtkinter.CTkOptionMenu):
     def __init__(self, master):
         super().__init__(master)
 
-        self.select_team_option = customtkinter.CTkOptionMenu(master, values=["Team1", "Team2", "Team3"],
+        self.select_client_option = customtkinter.CTkOptionMenu(master, values=[f"Client{x}" for x in range(1, 5)],
+                                                                anchor="center")
+        self.select_client_option.place(x=655, y=40)
+
+        self.select_team_option = customtkinter.CTkOptionMenu(master, values=[f"Team{x}" for x in range(1, 5)],
                                                               anchor="center")
-        self.select_team_option.place(x=655, y=40)
+        self.select_team_option.place(x=655, y=120)
 
         self.select_person_option = customtkinter.CTkOptionMenu(master, values=[f"Person{x}" for x in range(1, 20)],
                                                                 anchor="center")
-        self.select_person_option.place(x=655, y=120)
+        self.select_person_option.place(x=655, y=200)
 
         self.select_process_option = customtkinter.CTkOptionMenu(master, values=[f"Process{x}" for x in range(1, 20)],
-                                                                anchor="center")
-        self.select_process_option.place(x=655, y=200)
+                                                                 anchor="center")
+        self.select_process_option.place(x=655, y=280)
